35851ec64a4c05416356dc8d713f1d00c2a9d7bc
YuJunCheol <sinaburo0425@gmail.com>
2020-10-12 16:05:44 +0900

    2020_10_12 유아이만 구성

---
+++ MobilePTClient/app/src/main/java/com/example/mobileptclient/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.mobileptclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.example.mobileptclient.View_Controller.PagerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context mcontext;
+    ProgressBar pbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mcontext = getApplicationContext();
+
+        PagerAdapter sectionsPagerAdapter = new PagerAdapter(this, getSupportFragmentManager(),3);
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+}
---
+++ MobilePTClient/app/src/main/java/com/example/mobileptclient/View_Controller/WeightFragment.java
@@ -0,0 +1,27 @@
+package com.example.mobileptclient.View_Controller;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mobileptclient.R;
+
+public class WeightFragment extends Fragment {
+
+
+
+    public WeightFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weight, container, false);
+    }
+}
